9c7d2109d08ac8dc2d5c19b25a9eb8b7a5736993
Alexander <79529341506@yandex.ru>
2021-06-24 10:03:40 +0700

    setup fragments

---
+++ app/src/main/java/com/buslaev/workoutapp/screens/exercises/ExercisesFragment.kt
@@ -0,0 +1,20 @@
+package com.buslaev.workoutapp.screens.exercises
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.buslaev.workoutapp.R
+
+class ExercisesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_exercises, container, false)
+    }
+
+}
